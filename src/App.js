60875978983e17590b791617aaf55fe3fb01ac55
usha singh <ushasing215105@gmail.com>
2023-10-06 01:25:56 -0400

    new updates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
-
+import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
 
 function App() {
   return (
+    <>
+    <Router>
+      <Routes>
+         <Route path='/' exact element={<Home/>} />
+      </Routes>
+    </Router>
+   
     <h1 className="text-h1 font-bold text-red text-center">
       Hello world!
     </h1>
+    </>
   );
 }
 
